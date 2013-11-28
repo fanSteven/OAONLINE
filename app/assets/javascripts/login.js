457faa8aa9e2a54356aeddf535a55006d1c52821
fanjiaxing <fanjiaxing@hotmail.com>
2013-11-28 02:42:52 -0800

    indiiii

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -1,37 +1,12 @@
 /**
- * @author Steven
+ * Created with JetBrains RubyMine.
+ * User: steven
+ * Date: 11/27/13
+ * Time: 11:02 PM
+ * To change this template use File | Settings | File Templates.
  */
-var userinfoArray =
-    [
-        {"username":"itsm","password":"itsm"},
-        {"username":"admin","password":"admin"}
-    ];
-function submitlogin(){
-    var username = $("#username").val();
-    var password = $("#password").val();
-    var _ret = false;
-    var _cunzai = false;
-    var _resultStr = "";
-    //alert(username);
-    for (var i=0; i < userinfoArray.length; i++) {
-        if(userinfoArray[i].username==username && userinfoArray[i].password==password){
-            //alert(username);
-            _ret = false;
-            _cunzai = true;
-            $("#result_span").hide();
-            location.href = "index.html";
-            break;
-        }else{
-            _resultStr = "用户名或密码错误！";
-
-        }
-    }
-    $("#result_span").empty();
-    if(_resultStr!="" && !_cunzai){
-        $("#result_span").append(_resultStr);
-        $("#result_span").show();
-        _resultStr="";
-    }
-    return _ret;
+function userLogin()
+{
+    var username = $("#uname").val();
+    var userpass = $("#upassword").val();
 }
-
